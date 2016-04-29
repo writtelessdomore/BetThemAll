c494ce434a73f455ae233c1c68178632e5a270bd
léo <lo@leo-pc>
2016-04-29 17:02:38 +0200

    first commit

---
+++ src/main/java/doutreleau/leo/BetThemAllApplication.java
@@ -0,0 +1,13 @@
+package doutreleau.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetThemAllApplication {
+
+	public static void main(String[] args){
+		
+		SpringApplication.run(BetThemAllApplication.class, args);
+	}
+}
